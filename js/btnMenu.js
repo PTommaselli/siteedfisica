66052580fbe3fba55ad61bb3c6ef6f1e07345822
PTommaselli <pedrotommaselli@gmail.com>
2018-06-03 10:45:56 -0400

    233

---
+++ js/btnMenu.js
@@ -0,0 +1,7 @@
+function fnMenu() {
+    if(document.querySelector('#navegacao').style.display == 'none'){
+        document.querySelector('#navegacao').style.display = 'flex';
+    }else{
+        document.querySelector('#navegacao').style.display = 'none';
+    }
+}
